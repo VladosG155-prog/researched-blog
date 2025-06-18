cb34e94a009020d270e19762a14bc71d6589cb2b
Колос Владислав Игоревич <VKolos@alfabank.ru>
2025-06-19 02:35:59 +0300

    test

--- data/html/wp-config.php
+++ data/html/wp-config.php
@@ -98,7 +98,7 @@ define('COMPRESS_SCRIPTS', true);
 define('CONCATENATE_SCRIPTS', false);
 
 // === SSL ===
-define('FORCE_SSL_ADMIN', true);
+define('FORCE_SSL_ADMIN', false);
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
     $_SERVER['HTTPS'] = 'on';
 }
